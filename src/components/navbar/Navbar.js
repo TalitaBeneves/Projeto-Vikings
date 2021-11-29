94d9fd16ab9edd5c881e776812b7daa0c05bfd30
Talita Beneves <talitabeneves15@gmail.com>
2021-11-29 14:48:44 -0300

    navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MdOutlineMenu, AiOutlineClose } from 'react-icons/all';
+
+import Button from '../button/Button';
+
+import './navbar.css';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+  <>
+    <nav className="navbar">
+      <Link to='/' className="navbar-logo">
+          VIKNGS
+      </Link>
+      <div className="menu-icon" onClick={handleClick}>
+        {click ? <AiOutlineClose/> : <MdOutlineMenu />}
+      </div>
+      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+        <li className="nav-item">
+          <Link to="/sobre" className="nav-links" onClick={closeMobileMenu}>
+              HOME
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/sobre" className="nav-links" onClick={closeMobileMenu}>
+            SOBRE
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/personagens" className="nav-links" onClick={closeMobileMenu}>
+            PERSONAGENS
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/videos" className="nav-links" onClick={closeMobileMenu}>
+            VÍDEOS 
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/suporte" className="nav-links" onClick={closeMobileMenu}>
+              SUPORTE
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/sobre" className="nav-links" onClick={closeMobileMenu}>
+              MÍDIAS
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+            Entre 
+          </Link>
+        </li>
+      </ul>
+      <Button/>
+    </nav>
+  </>
+  )
+}
+
+export default Navbar;
