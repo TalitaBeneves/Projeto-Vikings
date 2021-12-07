6535f22825fa6135686f23bf40511b38f6f40ceb
Talita Beneves <talitabeneves15@gmail.com>
2021-12-06 23:01:10 -0300

    correção em trailers

--- src/components/trailer/Trailer.js
+++ src/components/trailer/Trailer.js
@@ -4,13 +4,17 @@ import Bjorn from '../../video/Bjorn.m4v';
 import Ivar from '../../video/Ivar.m4v';
 import Floki from '../../video/Floki.m4v';
 import Lagertha from '../../video/Lagertha.m4v';
+import logo from "../../img/download.png";
 
 import './trailer.css';
 
 const Trailer = () => {
   return (
     <>
-    <h1>Trailers</h1>
+    <div className="logo">
+    <img  src={logo} alt="logo"/>
+    </div>
+    <h1>Trailers</h1> <br />
     <div className="container__trailer" id="video1">
       <div className="trailer_video">
         <video controls src={Bjorn} type='video/mp4' id="video__bjorn" />
