8c7508b90223b99f788259e94ec096ff08012982
Talita Beneves <talitabeneves15@gmail.com>
2021-12-06 23:11:47 -0300

    hero update

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,24 +7,16 @@ const Hero = () => {
   return (
     <>
       <div className="hero" to="/">
-        <h2>Vikings</h2>
-        <p> navegadores nórdicos que <br/>exploram - e conquistam - novos territórios nos tempos medievais.</p>
-        <p>Primeiro episódio: 3 de março de 2013 <br/>
-          Episódio final: 30 de dezembro de 2020 <br/>
-          Gêneros: Guerra, Drama, Obra de Época, Aventura, Ficção de aventura, Ação <br/>
-          Emissoras originais: Amazon Prime Video, History
-          Spin-off: Vikings: Valhalla
-        </p>
         <div className="social">
           <ul>
             <li>
-              <a href="https://www.instagram.com/talita_beneves/"><FaInstagram /> </a>
+              <a  href="https://www.instagram.com/talita_beneves/" rel="noreferre" target="_blank" ><FaInstagram /> </a>
             </li>
             <li>
-              <a href="https://linkedin.com/in/talitabeneves/"><FaLinkedin /> </a>
+              <a href="https://linkedin.com/in/talitabeneves/" rel="noreferre" target="_blank" ><FaLinkedin /> </a>
             </li>
             <li>
-              <a href="https://github.com/TalitaBeneves"><FaGithub /> </a>
+              <a href="https://github.com/TalitaBeneves" rel="noreferre" target="_blank" ><FaGithub /> </a>
             </li>
           </ul>
         </div>
