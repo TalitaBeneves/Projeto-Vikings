18bf840165c54706e4f67f3c0b93962c8fb33fda
Talita Beneves <talitabeneves15@gmail.com>
2021-12-06 23:04:47 -0300

    update geral

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import Routes from './routes';
 
-import Navbar from "./components/navbar/Navbar";
-import Hero from '../src/components/hero/Hero';
-import Person from '../src/components/person/Person';
-import About from "./components/about/About";
-import Trailer from "./components/trailer/Trailer";
-import Footer from "./components/footer/Footer";
+
 
 function App() {
   return (
     <Router>
-      {/* <Navbar />
-      <hr/>
-      <Hero />
-      <hr/>
-      <Person /> <br />
-      <About /> <br />
-      <Trailer /> <br /> */}
-      <Footer />
+      <Routes />
     </Router>
   );
 }
