423390ad3f20ccb572c885f8c8efa365082022af
Talita Beneves <talitabeneves15@gmail.com>
2021-12-11 10:56:52 -0300

    FullPerson

---
+++ src/components/person/FullPerson.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './fullPerson.css';
+
+const FullPerson = () => {
+  return (
+  <>
+      <h1 className="h1_custon"> PERSONAGENS </h1> <br />
+    <div className="conteiner">
+    <div className="card" id="floki">
+      <div className="card-content">
+        <h2 className="card-title">Floki</h2> <br/>
+        <p className="card-body">
+          Floki Vilgerðarson é um construtor de barcos e trapaceiro incorrigível, que por acaso também é o excêntrico e amigo mais próximo de Ragnar Lothbrok. Ele é o marido de Helga e eles têm uma filha chamada Angrboda.
+        </p>
+      </div>
+      </div>
+      <div className="card" id="iva">
+        <div className="card-content">
+          <h2 className="card-title">Ivar</h2> <br/>
+          <p className="card-body">
+           Ivar, o Desossado, também chamado Ivar, Filho de Ragnar, foi líder Viking e comandante militar que invadiu o que é hoje a Inglaterra. Segundo a Saga de Ragnar Calças Peludas, foi filho de Ragnar Calças Peludas e Aslauga.
+          </p> 
+        </div>
+      </div>
+      <div className="card" id="bjorn">
+        <div className="card-content">
+          <h2 className="card-title">Bjorn</h2> <br/>
+          <p className="card-body">
+            Björn, também conhecido como Bjorn Ironside, é o Rei de Kattegat. Ele é filho de Ragnar Lothbrok e Lagertha e o mais velho dos muitos filhos de Ragnar.
+          </p>
+        </div>
+      </div>
+      <div className="card" id="ragnar">
+        <div className="card-content">
+          <h2 className="card-title">Ragnar</h2> <br/>
+          <p className="card-body">
+           Ragnar é um fazendeiro e guerreiro viking que anseia por saquear os rumores de riquezas da desconhecida Inglaterra. Sua primeira esposa é Lagertha, e eles têm dois filhos chamados Gyda e Bjorn. 
+          </p>
+        </div>
+      </div>
+      <div className="card" id="lagertha">
+        <div className="card-content">
+          <h2 className="card-title">Lagertha</h2> <br/>
+          <p className="card-body">
+            Lagertha é a primeira e ex-esposa de Ragnar Lothbrok e a ex-Rainha de Kattegat. Ela é a mãe de Björn. Lagertha é um conde e uma feroz donzela-escudo.
+          </p>
+        </div>
+      </div>
+    <div className="card" id="athelstan">
+      <div className="card-content">
+        <h2 className="card-title">Athelstan</h2> <br/>
+        <p className="card-body">
+         Athelstan é um jovem monge cristão anglo-saxão levado como escravo por Ragnar Lothbrok do mosteiro de Lindisfarne. Ele está dividido entre as crenças Viking e Cristãs, e se torna um conselheiro e amigo de Ragnar.
+        </p> 
+      </div>
+      </div>
+      <div className="card" id="rollo">
+        <div className="card-content">
+          <h2 className="card-title">Rollo</h2> <br/>
+          <p className="card-body">
+            Rollo é o irmão mais velho de Ragnar Lothbrok e duque da Normandia. Antes de seu casamento, Rollo era conhecido como um guerreiro viking temível, com uma tendência impulsiva e lutava por lutas internas.
+          </p> 
+        </div>
+      </div>
+      <div className="card" id="harald">
+        <div className="card-content">
+          <h2 className="card-title">King Harald</h2> <br/>
+          <p className="card-body">
+            Harald "Cabelo Fino" Halfdansson é um senhor da guerra e rei escandinavo. Seu sonho é se tornar o primeiro rei de toda a Noruega. Seu irmão é Halfdan, o Negro.
+          </p>
+        </div>
+      </div>
+      <div className="card" id="bispo">
+        <div className="card-content">
+          <h2 className="card-title">Bispo Heahmund</h2> <br/>
+          <p className="card-body">
+            O bispo Heahmund era um bispo saxão de Sherborne e um guerreiro sagrado.
+          </p>
+        </div>
+      </div>
+      <div className="card" id="siggy">
+        <div className="card-content">
+          <h2 className="card-title">Siggy</h2> <br/>
+          <p className="card-body">
+            Siggy é uma personagem principal da primeira, segunda e terceira temporada. Ela é interpretada por Jessalyn Gilsig e apareceu pela primeira vez em "Rites of Passage".
+          </p>
+        </div>
+      </div>
+      <div className="card" id="aslaug">
+        <div className="card-content">
+          <h2 className="card-title">Aslaug</h2> <br/>
+          <p className="card-body">
+           Aslaug, eram uma rainha na mitologia nórdica. Terceira esposa do Viking Ragnar Lothbrok. Pai – Siegfried – um dos heróis mais importantes da mitologia e épico alemão-escandinavo, o herói de “Canção sobre os Nibelungos”. 
+          </p>
+        </div>
+      </div>
+      <div className="card" id="torvi">
+        <div className="card-content">
+          <h2 className="card-title">Torvi</h2> <br/>
+          <p className="card-body">
+          Torvi é uma Dama do Escudo. Torvi se casa com Erlendur. Alguns anos depois, Torvi mata Erlendur e vai morar em Kattegat com seu amante e agora terceiro marido, Bjorn Ironside.
+          </p>
+        </div>
+      </div>
+      <div className="card" id="ubbe">
+        <div className="card-content">
+          <h2 className="card-title">Ubbe</h2> <br/>
+          <p className="card-body">
+           Ubbe Ragnarsson é o primeiro filho de Ragnar Lothbrok e Aslaug. Ele é o segundo filho mais velho de Ragnar. Ele eventualmente se casa com Torvi.
+          </p>
+        </div>
+      </div>
+      <div className="card" id="ecbert">
+        <div className="card-content">
+          <h2 className="card-title">Rei Ecbert</h2> <br/>
+          <p className="card-body">
+            O rei Ecbert Ealhmunding é o mundano e ambicioso rei de Wessex e da Mércia. Ele desenvolveu um grande respeito por seu novo inimigo e às vezes aliado Ragnar Lothbrok.
+          </p>
+        </div>
+      </div>
+      <div className="card" id="vidente">
+        <div className="card-content">
+          <h2 className="card-title">Vidente</h2> <br/>
+          <p className="card-body">
+            O Vidente era o Oráculo de Kattegat. Os videntes são adivinhos místicos com poderes sobrenaturais que podem se comunicar com os deuses e responder a perguntas sobre o futuro e o destino de alguém na vida.
+          </p>
+        </div>
+      </div>
+    </div>
+      <div className="button__person">
+        <Link to="/">
+          <button>Voltar</button>
+        </Link> 
+      </div>  
+      <br />
+      <br />
+  </>
+  )
+}
+
+export default FullPerson;
