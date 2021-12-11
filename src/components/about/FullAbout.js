a3113d2c91074f67cb45d2dbe5cab0310f781859
Talita Beneves <talitabeneves15@gmail.com>
2021-12-11 10:57:26 -0300

    FullAbout

---
+++ src/components/about/FullAbout.js
@@ -0,0 +1,156 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Hr from "../hr/Hr";
+
+import "./FullAbout.css";
+
+const FullAbout = () => {
+  return (
+    <>
+    <div>
+      <h1 className="h1_custon">A DESCOBERTA VIKING DA ISLÂNDIA</h1>
+      <p className="text_about">
+        o norueguês Floki Vilgerdarson partiu de Rogaland com a intenção de se
+        estabelecer em Snæland. Floki tinha a reputação de grande guerreiro
+        viking, mas era um colono sem esperanças. Floki passou seu verão caçando
+        focas em Vatnesfjörður em Breiðarfjörður no noroeste da Islândia, mas
+        ele se esqueceu de armazenar feno, o que fez com que todo o gado que
+        levou consigo morresse de fome no inverno. Isso condenou a sua tentativa
+        de colonização, mas o gelo do fiorde o impediu de voltar para casa.
+        Quando a camada de gelo finalmente se quebrou, já era tarde demais para
+        arriscar tentar retornar à Noruega, então Floki foi forçado a ficar mais
+        um inverno, desta vez em Borgarfjör furtherur, mais ao sul.
+        Completamente desiludido com as suas experiências, Floki decidiu
+        renomear Snæland como "Ísland". O nome de Floki foi o que pegou, embora
+        os seus homens dessem relatórios mais favoráveis sobre a ilha: o mais
+        entusiasmado deles, Thorolf, jurou que manteiga pingava de cada folha de
+        grama. Por esta razão, ela ficou conhecido desde então como Manteiga de
+        Thorolf.
+      </p>
+
+      <p className="text_about">
+        Thorolf deve ter sido um otimista inato. A Islândia é uma grande ilha
+        vulcânica situada exatamente na crista meso-atlântica, onde o magma que
+        jorra do manto está gradualmente separando a Europa da América. Apesar
+        de se situar apenas ao sul do Círculo Polar Ártico, a influência da
+        corrente quente do Golfo mantém o clima ameno para a latitude. As
+        geleiras e mantos de gelo nas montanhas cobrem cerca de 14% da Islândia,
+        mas o resto da ilha está livre de permafrost.
+      </p>
+
+      <p className="text_about">
+        A combinação de gelo e fogo da Islândia deve ter lembrado aos colonos do
+        mito da criação viking, no qual o mundo emerge do vazio entre o reino do
+        fogo de Muspel e o reino congelado de Niflheim.
+      </p>
+
+      <p className="text_about">
+        Hoje, menos de 1/4 da Islândia possui vegetação, o restante da área sem
+        glaciação é composto principalmente de campos de lava estéreis e
+        desertos de cinzas. No entanto, quando foi descoberta pelos vikings,
+        cerca de 40% da era coberto por bosques baixos e arbustos, de bétulas e
+        salgueiros, por isso teria parecido consideravelmente menos desolado do
+        que hoje. Mesmo assim, a Islândia acabou sendo um ambiente distintamente
+        marginal para a colonização europeia e os colonos eram muito vulneráveis
+        aos caprichos do clima e das erupções vulcânicas.
+      </p>
+
+      <p className="text_about">
+        Ouvindo os relatórios que circulavam sobre a Islândia, dois irmãos
+        adotivos noruegueses, Ingolf e Hjorleif, fizeram uma viagem de
+        reconhecimento aos fiordes orientais no final de 860 d.C. para avaliar
+        as perspectivas de assentamentos. Os irmãos haviam perdido suas
+        propriedades ao pagarem uma indenização ao Earl Atli da Gaular por
+        matarem seus filhos, portanto, precisavam urgentemente de um refúgio
+        seguro.
+      </p>
+      <br />
+      <Hr />
+      <iframe className="mapa"
+        src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d73617.75149283439!2d-6.562525400615374!3d54.787793616936646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x485e10ca99a69975%3A0xf7e528ef6eb7e3d8!2sIrlanda%20do%20Norte%2C%20Reino%20Unido!3m2!1d54.7877149!2d-6.4923145!5e0!3m2!1spt-BR!2sbr!4v1638990292928!5m2!1spt-BR!2sbr"
+        allowfullscreen=""
+        loading="lazy"
+        title="talta"
+        >  
+        </iframe>
+
+      <h1 className="h1_custon">Ragnar Lodbrok</h1>
+      <p className="text_about">
+        Várias tentativas foram feitas para identificar Regnar com os chefes
+        Viking históricos do século IX, mas embora a figura possa ter um núcleo
+        histórico frágil, a história de Regnar, como é contada por Saxo e na
+        saga loðbrókar de Ragnar islandês, deve ser considerada como um
+        conglomerado de lendas de diferentes origens.
+      </p>
+
+      <p className="text_about">
+        De acordo com Saxo e a saga, Regnar recebe seu apelido devido às calças
+        de couro peludo que usa quando derrota um dragão ou lindorm, que guarda
+        a filha do rei sueco Thora Borgarhjort. Thora então se torna a primeira
+        esposa de Regnar; sua segunda esposa é Lagertha, e sua terceira esposa
+        foi Aslauga.
+      </p>
+
+      <p className="text_about">
+        Regnar tem filhos que mais tarde se tornam guerreiros famosos. Regnar
+        devasta ao longo das costas inglesas e francesas e realiza muitos
+        feitos, mas termina seus dias na fazenda de minhocas do rei inglês Ella.
+      </p>
+
+      <p className="text_about">
+        Ragnar Lodbrok é apresentado pelo cronista dinamarquês Saxão Gramático,
+        no século XIII, como sendo jarl do rei Horik 1. da Dinamarca, embora
+        tendo origem na dinastia sueca dos Inglingos. Tanto Saxão quanto a saga
+        islandesa Saga de Ragnar Lodbrok descrevem Ragnar como filho do
+        igualmente lendário Sigurdo, o Anel, um rei da Suécia que teria
+        conquistado a Dinamarca. Todavia, os cronistas não concordam com o local
+        de residência principal de Ragnar, se a Suécia ou a Dinamarca.
+      </p>
+
+      <p className="text_about">
+        Segundo a lenda, Ragnar foi casado três vezes: com a skjaldmö Lagertha,
+        com a bela Tora Borgarhjort, e com Aslauga. Diz-se ter sido parente do
+        rei dinamarquês Godofredo e filho do rei sueco Sigurdo, o Anel.
+        Tornou-se rei e distinguiu-se por muitas invasões e conquistas. Há duas
+        histórias diferentes sobre sua morte. Uma delas é que ele foi capturado
+        por seu inimigo, o rei Ela da Nortúmbria, e morto ao ser jogado em um
+        poço cheio de cobras. Seus filhos o vingaram invadindo a Inglaterra com
+        o Grande Exército Pagão. A outra foi que Ragnar morreu de uma combinação
+        de cólera e ferimentos que sofreu ao tentar invadir Paris.
+      </p>
+      
+      <div className="container_about">
+        <div className="info">
+          <h3 className="title_custon">Esposas</h3>
+          <h5>Lagertha, Tora Borgarhjört, Aslauga</h5>
+
+          <h3 className="title_custon">Descendência</h3>
+          <ul>
+            <li>Ivar, o Desossado</li>
+            <li> Biorno Flanco de Ferro</li>
+            <li> Haldano</li>
+            <li> Sigurdo Serpente no Olho</li>
+            <li> Ubba Ragnarsson</li>
+            <li> Hivtserk Camisa Branca</li>
+            <li> Fridleif</li>
+          </ul>
+
+          <h3 className="title_custon">Pai</h3>
+          <h5>Sigurdo, o Anel</h5>
+
+          <h3 className="title_custon">Religião</h3>
+          <h5>Religião nórdica</h5>
+        </div>
+      </div>
+      </div>
+      <div className="button__person">
+        <Link to="/">
+          <button>Voltar</button>
+        </Link> 
+    </div>
+  </>
+  );
+};
+
+export default FullAbout;
